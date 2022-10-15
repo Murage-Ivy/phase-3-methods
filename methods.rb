62282534ccf864ca3f5f032c615035bb815819a7
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-15 13:04:16 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+# greet_programmer
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+# greet("Programmer")
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+# greet_with_default("Naureen")
+
+def add(num1, num2)
+  num1 + num2
+end
+
+# sum = add(1, 2)
+# puts sum
+
+def halve(number)
+  if (number.class != Integer)
+    return nil
+  end
+  return number / 2
+end
+
+# puts halve(9)
